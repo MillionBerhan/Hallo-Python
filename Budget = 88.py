0e4b3a229b8f76344058a4dd947e45cf6d82951d
MILLION BERHAN <99065113@mydavinci.com>
2021-09-09 10:23:44 +0200

    Zip mijn-gegevens

---
+++ Budget = 88.py
@@ -0,0 +1,8 @@
+print("De feestlunch kost je bij de bakker 18.88 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
+print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro")
+amount = 555
+print(amount)
+6.34 * str (amount)
+'6.34' + int(amount)
+
+
